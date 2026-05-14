76d3c81d8590bfa21aa946c6ff513fd316a82c70
praskumar005 <17404ec005@gmail.com>
2026-05-15 04:34:24 +0530

    Update main.tf

--- AWS EC2 Instance/main.tf
+++ AWS EC2 Instance/main.tf
@@ -7,6 +7,8 @@ resource "aws_instance" "ec2" {
   ami           = "ami-0cca150d127c2216f"
   instance_type = "t2.micro"
   key_name      = "OSI_Key"
+  availability_zone = "us-east-1a"
+  count = 1 #Specify as per the requirement
   tags = {
     Name = "Test-Instance"
   }
