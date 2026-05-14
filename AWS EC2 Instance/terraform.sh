242e1461a3559da4a22dd5bf9291fc10f36ef419
praskumar005 <17404ec005@gmail.com>
2026-05-15 03:50:26 +0530

    Create terraform.sh

---
+++ AWS EC2 Instance/terraform.sh
@@ -0,0 +1,26 @@
+# Ubuntu/Debian
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+
+# CentOS/RHEL
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install terraform
+
+# Amazon Linux
+sudo yum install -y yum-utils shadow-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum install terraform
+
+# To install the AWS CLI, run the following commands.
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Configure the AWS CLI
+$ aws configure
+AWS Access Key ID [None]: "Access-key"
+AWS Secret Access Key [None]: "Secret-Key"
+Default region name [None]: "Region-Name" # Optional-> Enter to skip
+Default output format [None]: "json" # Optional-> Enter to skip
