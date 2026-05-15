1619d81f2e236fafad9107cfc889cf54ff13fa24
praskumar005 <17404ec005@gmail.com>
2026-05-16 02:54:59 +0530

    Update main.tf

--- Variables/main.tf
+++ Variables/main.tf
@@ -5,7 +5,7 @@ region = "us-east-1"
 resource "aws_instance" "ec2" {
 
 ami = var.ami_id
-instance_type = var.instance_type ? t2.micro : t2.medium
+instance_type = var.instance_type ? "t2.micro" : "t2.medium"
 key_name = var.key_name
 tags = {
 Name = "Terraform-Instance"
