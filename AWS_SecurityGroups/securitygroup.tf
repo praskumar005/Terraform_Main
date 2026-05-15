805580a591017ec26cb586f05bff0562fecbd556
praskumar005 <17404ec005@gmail.com>
2026-05-16 01:49:22 +0530

    Update securitygroup.tf

--- AWS_SecurityGroups/securitygroup.tf
+++ AWS_SecurityGroups/securitygroup.tf
@@ -1,1 +1,45 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_security_group" "aws_sg" {
+
+  name        = "terraform-sg"
+  description = "This SG is for terraform created instances"
+
+  # Inbound Rule for SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Inbound Rule for HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow All Traffic
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound Rule
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-sg"
+  }
+}
